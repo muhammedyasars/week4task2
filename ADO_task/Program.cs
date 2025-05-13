56830b25420a48e55f4c0523a9ad2b8148b83a9a
Muhammed Yasar S <muhammedyasars810@gmail.com>
2025-05-13 12:46:04 +0530

    Update Program.cs

--- ADO_task/Program.cs
+++ ADO_task/Program.cs
@@ -11,19 +11,19 @@ namespace StudentDataApp
         {
             StudentRepository repo = new StudentRepository();
 
-            // Get data
+    
             DataSet dataSet = repo.GetStudents();
 
-            // Display original
+            
             repo.DisplayAllStudents(dataSet);
 
-            // Modify Age of student with Id = 1 to Age = 25
+       
             repo.UpdateStudentAge(dataSet, 1, 25);
 
-            // Display updated data
+    
             repo.DisplayAllStudents(dataSet);
 
-            // Filter and show students of age 25
+  
             repo.DisplayFilteredStudentsByAge(dataSet, 25);
         }
     }
